54b251b16b66b70b952e0e29ee04c59b6c91a5c3
Alexander Molchanov <bonext@gmail.com>
2016-08-01 07:41:34 -0400

    theano function scaffold?

--- test-tdeconv.py
+++ test-tdeconv.py
@@ -9,37 +9,26 @@ input = np.random.rand(2, 2, 4, 6).astype(theano.config.floatX)
 in_shape = input.shape
 neib_shape = [2, 3]
 
-i2n_in = T.tensor4()
-t_get_spatial_ns = theano.function(
-    [i2n_in],
-    images2neibs(i2n_in, neib_shape)
-)
+# lets do it all in one step
+pool_shape = [2, 2, 3] # channels x h x w
 
-output_spatial = t_get_spatial_ns(input)
-print output_spatial.shape
+pool_in = T.tensor4() # pool input batch x channels x H x W
 
-#print input
-#print "---"
-#for i in range(output_spatial.shape[0]):
-#    print output_spatial[i, :]
+# collect spatial neighbourhoods
+sp_pool = images2neibs(pool_in, pool_shape[-2:])
+
+# spatial lines per channel - can be prepared in advance
+lpc = np.prod(input.shape[-2:])/np.prod(pool_shape[-2:])
+
+# collect channels in a separate dimension
+sp_pool_shape = () # (sp_pool.shape[0]/lpc, lpc, sp_pool.shape[-1])
+sp_collect_chans = sp_pool.reshape(sp_pool_shape)
+next_in = T.shape_padleft(sp_collect_chans.transpose(1,0,2))
+
+# pool by channels
+pooling_chunks = images2neibs(next_in, [pool_shape[0], next_in.shape[-1]])
+
+# now each line of pooling chunks can be max-abs and argmax-abs
+# we will need reshape to get actual pooling result from it
 
 
-# output_spatial lines per channel
-lpc = np.prod(input.shape[-2:])/np.prod(neib_shape)
-
-next_in = output_spatial.reshape(output_spatial.shape[0]/lpc, lpc, output_spatial.shape[-1])
-next_in = next_in.transpose(1,0,2)
-
-c_shape = [2, next_in.shape[-1]]
-t_get_ch_ns = theano.function(
-    [i2n_in],
-    images2neibs(i2n_in, c_shape)
-)
-
-out_ch = t_get_ch_ns(next_in[np.newaxis,:,:,:])
-
-print input
-print "---"
-#print next_in
-for i in range(out_ch.shape[0]):
-    print out_ch[i]
