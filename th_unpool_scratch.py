85fec6eb5d71470f63d134513b6a78f30de7bc9a
Alexander Molchanov <bonext@gmail.com>
2016-08-16 13:30:14 +0300

    unpool almost done, need check

--- th_unpool_scratch.py
+++ th_unpool_scratch.py
@@ -1,7 +1,7 @@
 import numpy as np
 import theano
 import theano.tensor as T
-from theano.tensor.nnet.neighbours import images2neibs
+from theano.tensor.nnet.neighbours import neibs2images
 floatX = theano.config.floatX
 
 from deconv import Pooling
@@ -41,6 +41,12 @@ def t_mk_unpool(t_unpool_input, t_sp_pool_dims, t_ch_pool_sz, t_switches):
     t_raw_out = T.inc_subtensor(t_raw_out_init[t_rows, t_switches], t_pooled_raw)
 
     # now we only need to reshape back
+    t_out_prep_shape_1 = T.stack([1, t_in_h * t_in_w, t_batch_sz * t_out_ch, t_pool_h * t_pool_w])
+    t_out_prep_1 = neibs2images(t_raw_out, T.stack([t_pool_ch, t_pool_h*t_pool_w]), t_out_prep_shape_1)
+    t_out_prep_2 = T.transpose(t_out_prep_1[0], [1, 0, 2])
+    t_out_prep_shape_3 = T.stack([t_batch_sz*t_out_ch*t_in_h*t_in_w, t_pool_h*t_pool_w])
+    t_out_prep_3 = T.reshape(t_out_prep_2, t_out_prep_shape_3)
+    t_unpool_out_shape = T.stack([t_batch_sz, t_out_ch, t_out_h, t_out_w])
+    t_unpool_out = neibs2images(t_out_prep_3, T.stack([t_pool_h, t_pool_w]), t_unpool_out_shape)
 
-
-    return (t_raw_out)
+    return (t_unpool_out)
