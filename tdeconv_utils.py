b7037d577b8a8c1cb04289e5d2574f37d3fbb7cf
Alexander Molchanov <bonext@gmail.com>
2016-08-18 18:18:27 +0300

    Tests and docs
    + Added a test for theano convolution
    + Added docs on FISTA

--- tdeconv_utils.py
+++ tdeconv_utils.py
@@ -49,18 +49,26 @@ def t_mk_pool_ready(t_pool_input, t_pool_shape):
     t_pool_h = t_pool_shape[1]
     t_pool_w = t_pool_shape[2]
     # output
-    t_out_ch = t_in_ch // t_pool_ch
-    t_out_h = t_in_h // t_pool_h
-    t_out_w = t_in_w // t_pool_w
+    t_out_ch = (t_in_ch + t_pool_ch - 1) // t_pool_ch
+    t_out_h = (t_in_h + t_pool_h - 1) // t_pool_h
+    t_out_w = (t_in_w + t_pool_w - 1) // t_pool_w
+
+    # we will need to pad input (probably), so here's the padded shape:
+    t_padded_ch = t_out_ch * t_pool_ch
+    t_padded_h = t_out_h * t_pool_h
+    t_padded_w = t_out_w * t_pool_w
+    t_padded_pool_in_z = T.zeros(T.stack([t_batch_sz, t_padded_ch, t_padded_h, t_padded_w]))
+    t_padded_pool_in = T.inc_subtensor(t_padded_pool_in_z[:t_batch_sz, :t_in_ch, :t_in_h, :t_in_w], t_pool_input)
+
     # below is all computed
     # spatial pooling
-    t_sp_pooled = images2neibs(t_pool_input, T.stack([t_pool_h, t_pool_w]))
+    t_sp_pooled = images2neibs(t_padded_pool_in, T.stack([t_pool_h, t_pool_w]))
     # spatial pooling output shape
     # has size (B * C * H/h * W/w) x (h*w)
     t_sp_pooled_dims = t_sp_pooled.shape
     # lines per channel
     # H*W / (h*w)
-    t_lpc = (t_in_h * t_in_w) // (t_pool_h * t_pool_w)
+    t_lpc = (t_padded_h * t_padded_w) // (t_pool_h * t_pool_w)
     # shape to collect channels
     t_ch_pool_prep_dims_1 = T.stack([t_sp_pooled_dims[0] // t_lpc, t_lpc, t_sp_pooled_dims[1]])
     # preparing pooling by channels
@@ -70,7 +78,7 @@ def t_mk_pool_ready(t_pool_input, t_pool_shape):
     # prepare for channel pooling
     t_ch_pool_dims = T.stack([t_pool_ch, t_ch_pool_prep_dims_1[-1]])
     t_pool_ready = images2neibs(t_ch_pool_prep_2, t_ch_pool_dims)
-    return t_batch_sz, t_out_ch, t_out_h, t_out_w, t_pool_ready
+    return t_batch_sz, t_in_ch, t_in_h, t_in_w, t_out_ch, t_out_h, t_out_w, t_pool_ready
 
 
 def t_mk_reshape_pooled(t_batch_sz, t_out_ch, t_out_h, t_out_w, t_pooled_raw):
@@ -99,7 +107,7 @@ def t_mk_pooling(t_pool_input, t_pool_shape):
     :param t_pool_shape: lvector with pooling shape c x h x w
     :return: pooled 4d tensor, switch locations
     """
-    t_batch_sz, t_out_ch, t_out_h, t_out_w, t_pool_ready = t_mk_pool_ready(t_pool_input, t_pool_shape)
+    t_batch_sz, t_in_ch, t_in_h, t_in_w, t_out_ch, t_out_h, t_out_w, t_pool_ready = t_mk_pool_ready(t_pool_input, t_pool_shape)
 
     # argmax-pooling
     t_switches = T.argmax(abs(t_pool_ready), axis=1)
@@ -107,8 +115,8 @@ def t_mk_pooling(t_pool_input, t_pool_shape):
     t_pooled_raw = t_pool_ready[t_rows, t_switches]
 
     t_pool_out = t_mk_reshape_pooled(t_batch_sz, t_out_ch, t_out_h, t_out_w, t_pooled_raw)
-
-    return (t_pool_out, t_switches)
+    t_orig_shape = T.stack([t_batch_sz, t_in_ch, t_in_h, t_in_w])
+    return t_pool_out, t_switches, t_orig_shape
 
 
 def t_mk_fixed_pooling(t_pool_input, t_pool_shape, t_switches):
@@ -119,17 +127,17 @@ def t_mk_fixed_pooling(t_pool_input, t_pool_shape, t_switches):
     :param t_switches:
     :return: pooled 4d tensor, input switches
     """
-    t_batch_sz, t_out_ch, t_out_h, t_out_w, t_pool_ready = t_mk_pool_ready(t_pool_input, t_pool_shape)
+    t_batch_sz, t_in_ch, t_in_h, t_in_w, t_out_ch, t_out_h, t_out_w, t_pool_ready = t_mk_pool_ready(t_pool_input, t_pool_shape)
 
     t_rows = T.arange(t_pool_ready.shape[0])
     t_pooled_raw = t_pool_ready[t_rows, t_switches]
 
     t_pool_out = t_mk_reshape_pooled(t_batch_sz, t_out_ch, t_out_h, t_out_w, t_pooled_raw)
+    t_orig_shape = T.stack([t_batch_sz, t_in_ch, t_in_h, t_in_w])
+    return t_pool_out, t_switches, t_orig_shape
 
-    return (t_pool_out, t_switches)
 
-
-def t_mk_unpool(t_unpool_input, t_pool_shape, t_switches):
+def t_mk_unpooling(t_unpool_input, t_pool_shape, t_switches, t_orig_shape):
     """
     Make theano graph for unpooling with known switches
     :param t_unpool_input:
@@ -147,10 +155,14 @@ def t_mk_unpool(t_unpool_input, t_pool_shape, t_switches):
     t_pool_ch = t_pool_shape[0]
     t_pool_h = t_pool_shape[1]
     t_pool_w = t_pool_shape[2]
-    # output
-    t_out_ch = t_in_ch * t_pool_ch
-    t_out_h = t_in_h * t_pool_h
-    t_out_w = t_in_w * t_pool_w
+    # padded output
+    t_out_ch_padded = t_in_ch * t_pool_ch
+    t_out_h_padded = t_in_h * t_pool_h
+    t_out_w_padded = t_in_w * t_pool_w
+    # cropped output
+    t_out_ch = t_orig_shape[1]
+    t_out_h = t_orig_shape[2]
+    t_out_w = t_orig_shape[3]
 
     # reshape input to get ready for unpool
     t_lpsb = t_batch_sz * t_in_ch
@@ -162,12 +174,14 @@ def t_mk_unpool(t_unpool_input, t_pool_shape, t_switches):
     t_raw_out = T.inc_subtensor(t_raw_out_init[t_rows, t_switches], t_pooled_raw)
 
     # now we only need to reshape back
-    t_out_prep_shape_1 = T.stack([1, t_in_h * t_in_w, t_batch_sz * t_out_ch, t_pool_h * t_pool_w])
+    t_out_prep_shape_1 = T.stack([1, t_in_h * t_in_w, t_batch_sz * t_out_ch_padded, t_pool_h * t_pool_w])
     t_out_prep_1 = neibs2images(t_raw_out, T.stack([t_pool_ch, t_pool_h*t_pool_w]), t_out_prep_shape_1)
     t_out_prep_2 = T.transpose(t_out_prep_1[0], [1, 0, 2])
-    t_out_prep_shape_3 = T.stack([t_batch_sz*t_out_ch*t_in_h*t_in_w, t_pool_h*t_pool_w])
+    t_out_prep_shape_3 = T.stack([t_batch_sz*t_out_ch_padded*t_in_h*t_in_w, t_pool_h*t_pool_w])
     t_out_prep_3 = T.reshape(t_out_prep_2, t_out_prep_shape_3)
-    t_unpool_out_shape = T.stack([t_batch_sz, t_out_ch, t_out_h, t_out_w])
-    t_unpool_out = neibs2images(t_out_prep_3, T.stack([t_pool_h, t_pool_w]), t_unpool_out_shape)
+    t_unpool_out_shape = T.stack([t_batch_sz, t_out_ch_padded, t_out_h_padded, t_out_w_padded])
+    t_unpool_out_padded = neibs2images(t_out_prep_3, T.stack([t_pool_h, t_pool_w]), t_unpool_out_shape)
+    # cut original data
+    t_unpool_out = t_unpool_out_padded[:t_batch_sz, :t_out_ch, :t_out_h, :t_out_w]
 
     return t_unpool_out
---
+++ tests/tdeconv_tests.py
@@ -0,0 +1,79 @@
+from nose.tools import *
+
+import numpy as np
+import theano
+import theano.tensor as T
+
+
+class TestTheanoConvolutions(object):
+    def setUp(self):
+        self.filters = np.array(
+            [[[[1, 0, 0],
+               [0, 0, 0],
+               [0, 0, 0]]],
+             [[[0, 0, 0],
+               [0, 0, 0],
+               [0, 0, -1]]]
+             ],
+            dtype=theano.config.floatX
+        )
+
+    def test_theano_convolution(self):
+        # how to use t_mk_conv
+        from tdeconv_utils import t_mk_conv
+        in4 = T.tensor4(name='conv_in', dtype=theano.config.floatX)
+        f4 = T.tensor4(name='filters', dtype=theano.config.floatX)
+        f_conv = theano.function(
+            [in4],
+            t_mk_conv(in4, f4),
+            givens=[(f4, self.filters)]
+        )
+
+        test_input = np.array(
+            [[[[0, 0, 0, 0, 0],
+               [0, 0, 1, 0, 0],
+               [0, 1, 1, 1, 0],
+               [0, 0, 1, 0, 0],
+               [0, 0, 0, 0, 0]]]],
+            dtype=theano.config.floatX
+        )
+        ground_truth = np.array(
+            [[[[1,  1,  0],
+               [1,  0,  0],
+               [0,  0,  0]],
+              [[0,  0,  0],
+               [0,  0, -1],
+               [0, -1, -1]]]],
+            dtype=theano.config.floatX
+        )
+        assert_true(np.all(f_conv(test_input) == ground_truth))
+
+    def test_theano_transposed_convolution(self):
+        # how to use t_mk_conv_transpose
+        from tdeconv_utils import t_mk_conv_transpose
+        in4 = T.tensor4(name='conv_in', dtype=theano.config.floatX)
+        f4 = T.tensor4(name='filters', dtype=theano.config.floatX)
+        f_t_conv = theano.function(
+            [in4],
+            t_mk_conv_transpose(in4, f4),
+            givens=[(f4, self.filters)]
+        )
+
+        test_input = np.array(
+            [[[[0, 1, 0],
+               [0, 1, 0],
+               [0, 1, 0]],
+              [[0, 0, 0],
+               [1, 1, 1],
+               [0, 0, 0]]]],
+            dtype=theano.config.floatX
+        )
+        ground_truth = np.array(
+            [[[[ 0,  0,  0,  0,  0],
+               [-1, -1, -1,  0,  0],
+               [ 0,  0,  0,  1,  0],
+               [ 0,  0,  0,  1,  0],
+               [ 0,  0,  0,  1,  0]]]],
+            dtype=theano.config.floatX
+        )
+        assert_true(np.all(f_t_conv(test_input) == ground_truth))
