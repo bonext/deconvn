ae49f4963d0bc99bb15b4ac7fb830ed9be49b484
Alexander Molchanov <bonext@gmail.com>
2016-08-19 13:23:09 +0300

    Moved stuff around to make a package in future

---
+++ setup.py
@@ -0,0 +1,19 @@
+# try:
+#     from setuptools import setup
+# except ImportError:
+#     from distutils.core import setup
+#
+# config = {
+#     'description': 'Unsupervised hierarchical features.',
+#     'author': 'Alexander Molchanov',
+#     'url': 'https://github.com/bonext/deconvn',
+#     'download_url': '',
+#     'author_email': 'bonext@gmail.com',
+#     'version': '0.1',
+#     'install_requires': ['nose'],
+#     'packages': ['deconvn'],
+#     'scripts': [],
+#     'name': 'DeconvolutionalNetworks'
+# }
+#
+# setup(**config)
